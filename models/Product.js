c3ac2b1966bcdef130b3fbc1d88fb7a87f3bb482
ktotah <ket2137@columbia.edu>
2024-05-18 13:22:36 -0700

    Define Product model with id, product_name, price, stock, and category_id fields

--- models/Product.js
+++ models/Product.js
@@ -6,11 +6,43 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Define the Product model with id, product_name, price, stock, and category_id fields
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
+  // Set up the model to use the appropriate Sequelize settings
   {
     sequelize,
     timestamps: false,
