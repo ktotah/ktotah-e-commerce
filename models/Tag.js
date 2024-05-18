9974ce9a86c55f4e6b235c8167826178c7b13d11
ktotah <ket2137@columbia.edu>
2024-05-18 13:23:36 -0700

    Define Tag model with id and tag_name fields

--- models/Tag.js
+++ models/Tag.js
@@ -1,13 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Tag extends Model {}
 
+// Define the Tag model with id and tag_name fields
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
+  // Set up the model to use the appropriate Sequelize settings
   {
     sequelize,
     timestamps: false,
